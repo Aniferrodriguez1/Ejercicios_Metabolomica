373101e68c43d3b2f42a81acb42f56bcf076d4aa
Aniferrodriguez1 <165961687+Aniferrodriguez1@users.noreply.github.com>
2024-04-03 18:52:57 -0600

    G

---
+++ Gráfica de dispersión DesnutridosEunutridos.R
@@ -0,0 +1,136 @@
+#Creadora: Ana Fernanda Luna
+#Gráfica de puntos
+#Se realiza una serie de experimentos comparativos de 
+#cerebros de tres animales destetados cuyas madres estuvieron 
+#expuestas a una dieta restrictivas (desnutridas) y se compararon 
+#con tres animales cuyas madres se expusieron a una dieta ad libitum (eunutridas). 
+#Se desea saber: ¿Qué miRNAs fueron los que más cambiaron entre los animales desnutridos (tratamiento) 
+#comparados con los animales eunutridos (control)?
+
+install.packages("pacman")
+library(pacman)
+p_load("readr", #para lamar las bases de datos
+       "ggplot2", #para graficar
+       "ggrepel", #para etiquetar datos en una
+       "dplyr") #facilita el manejo de datos
+
+datos <- read_csv(file="https://raw.githubusercontent.com/ManuelLaraMVZ/Transcript-mica/main/DesnutridasvsEunutridas.csv)
+datos
+
+#extracción de genes controles (referencia)
+controles <- datos %>% 
+  filter(Condicion=="Control")
+head(controles)
+
+#sacar promedios de los controles 
+promedio_controles <-  controles %>% 
+  summarise(Mean_C1 = mean(Cx1),
+            Mean_C2 = mean(Cx2),
+            Mean_C3 = mean(Cx3),
+            Mean_T1 = mean(T1),
+            Mean_T2 = mean(T2),
+            Mean_T3 = mean(T3))
+mutate(Gen="promedio_controles") %>% 
+  select(7,1,2,3,4,5,6)
+promedio_controles
+
+#######
+#extraer los genes de la tabla de "datos
+
+genes <- datos %>% 
+  filter(Condicion=="Target") %>%
+  select(-2)
+head(genes)
+
+##############################
+
+#Sacar el 2^-DCT
+
+DCT <- genes %>% 
+  mutate(DCT_C1=2^-(Cx1-promedio_controles$Mean_C1),
+         DCT_C2=2^-(Cx2-promedio_controles$Mean_C2),
+         DCT_C3=2^-(Cx3-promedio_controles$Mean_C3),
+         DCT_T1=2^-(T1-promedio_controles$Mean_T1),
+         DCT_T2=2^-(T2-promedio_controles$Mean_T2),
+         DCT_T3=2^-(T3-promedio_controles$Mean_T3)) %>%
+  select(-2,-3,-4,-5,-6,-7)
+DCT
+
+promedio_genes <- DCT %>%
+  mutate(Mean_DCT_Cx=(DCT_C1+DCT_C2+DCT_C3)/3,
+         Mean_DCT_Tx=(DCT_T1+DCT_T2+DCT_T3)/3)
+
+promedio_genes
+
+#Grafica 
+
+grafica_dispersion <- ggplot(promedio_genes,
+                       aes(x=Mean_DCT_Cx,
+                           y=Mean_DCT_Tx))+
+  geom_point(size=2, 
+             color="magenta")
+grafica_dispersion
+
+grafica_dispersion2 <- grafica_dispersión +
+  labs(
+    title = "Condición control vs tratamiento",
+    caption = "Creador Ana Luna",
+    x = expression("Control 2-DCT"),
+    y = expression("Tratamiento 2-DCT")
+  ) +
+  geom_smooth(method = "lm", color = "pink") +
+  theme_minimal() + # Estilo de la gráfica
+  theme(
+    panel.background = element_rect(fill = "white"), # Fondo blanco
+    panel.grid.major = element_blank(), # Eliminar las líneas principales de la grilla
+    axis.text = element_text(family = "Times", size = 12), # Texto de ejes tamaño 12 tipo Times
+    axis.title = element_text(family = "Times", size = 14, face = "bold"), # Título de ejes
+    legend.title = element_text(family = "Times", size = 14), # Título de la leyenda
+    legend.text = element_text(family = "Times", size = 14) # Texto de la leyenda
+  )
+    
+grafica_dispersion2  
+
+################################
+
+#Identificación de genes
+head(promedio_genes)
+
+top_10 <- promedio_genes %>%
+  select(1,8,9) %>%
+  top_n(10, Mean_DCT_Cx) %>%
+  arrange(desc(Mean_DCT_Cx))
+
+head(top_10)
+
+grafica_dispersion3 <- grafica_dispersion2 +
+  geom_label_repel( data=top_10, #Usar el data frame con los 10 mejores registros
+    aes(x = Mean_DCT_Cx, 
+        y = Mean_DCT_Tx, 
+        label = Gen),
+    label.padding = unit(0.2, "lines")
+    max.overlaps = Inf # Permite un número infinito de superposiciones
+  )
+grafica_dispersion3
+
+ggsave
+ggsave("grafica_dispersion3.png",  # Nombre del archivo
+       plot = grafica_dispersion3,  # El gráfico a guardar
+       width = 7,  # Ancho del gráfico en unidades (pulgadas por defecto)
+       height = 5,  # Altura del gráfico en unidades
+       dpi = 300)  # Resolución en puntos por pulgada
+
+#¿Qué miRNAs fueron los que más cambiaron entre los animales 
+#desnutridos (tratamiento) comparados con los animales eunutridos (control)?
+
+#Los datos que más cambiaron serían aquellos en el Dataframe top_10
+#estos representan la media de la expresión en el grupo control
+#aunque típicamente se buscaría la comparación directa entre tratamiento
+#y control para determinar el cambio 
+
+#En este caso se visualizan en la gráfica estos miRNAs en la gráfica
+#de dispersión 3 donde los puntos más relevantes se encuentran etiquetados. 
+
+
+
+
